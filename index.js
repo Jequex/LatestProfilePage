d8b19854d59c524914fde3ca465f7484527367b6
jequex <onyejekwe.harddisk20@gmail.com>
2021-01-27 14:56:00 +0100

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const menuShow = document.querySelector('.links');
 const line2 = document.querySelector('.line2');
 const line1 = document.querySelector('.line1');
 const line3 = document.querySelector('.line3');
-const link = document.querySelectorAll('.link');
+const link = document.querySelectorAll('.links a');
 
 const toggle = () => {
     menuShow.classList.toggle('links-active')
     line2.classList.toggle('line2-active')
     line1.classList.toggle('line1-active')
     line3.classList.toggle('line3-active')
+    link.forEach((linker, index) => {
+        if (linker.style.animation) {
+            linker.style.animation = ''
+        } else {
+            linker.style.animation = `linksFade 0.5s ease forwards ${index/3}s`
+        }
+    })
 }
 
 link.forEach((linker) => {
