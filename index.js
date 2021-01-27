ed325d3d4d9d54fae1fae6520b32360f053579f1
jequex <onyejekwe.harddisk20@gmail.com>
2021-01-27 14:25:51 +0100

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const menuShow = document.querySelector('.links');
 const line2 = document.querySelector('.line2');
 const line1 = document.querySelector('.line1');
 const line3 = document.querySelector('.line3');
-burger.addEventListener('click', () => {
+const link = document.querySelectorAll('.link');
+
+const toggle = () => {
     menuShow.classList.toggle('links-active')
     line2.classList.toggle('line2-active')
     line1.classList.toggle('line1-active')
     line3.classList.toggle('line3-active')
+}
+
+link.forEach((linker) => {
+    linker.addEventListener('click', toggle)
 })
+
+burger.addEventListener('click', toggle)
